41837e4f79d43f7ec286b98981f1d4cea13668ee
Sergey Zubov <sergei@maground.com>
2019-12-06 17:51:29 +0300

    fix formatting

--- src/Mosaic.tsx
+++ src/Mosaic.tsx
@@ -1,21 +1,72 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   images: Array<string>;
+  gridColumns: number;
+  gridGutter: number;
 }
 
-export const Mosaic: React.FunctionComponent<Props> = ({images}) => (
-  <div>
-    {images.map((el, i) => [
-      <img
-        style={{
-          width: '100%',
-        }}
-        src={`https://source.unsplash.com/1600x${900 + i}`}
-        title="Sample title"
-        alt={'hello'}
-      />,
-      <br />,
-    ])}
-  </div>
-);
+const Images = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+interface ItemProps {
+  readonly width: number;
+  readonly padding: string;
+  readonly height: number;
+}
+
+const Item = styled.div<ItemProps>`
+  color: white;
+  position: relative;
+  box-sizing: border-box;
+  width: ${props => props.width}%;
+  margin: ${props => props.padding};
+
+  > div {
+    height: ${props => props.height}px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover !important;
+      overflow: hidden !important;
+    }
+  }
+`;
+
+export const Mosaic: React.FunctionComponent<Props> = ({
+  images,
+  gridColumns,
+  gridGutter,
+}) => {
+  const gutter = gridGutter / 2;
+  const width = (100 - (gridColumns - 1) * gutter * 2) / gridColumns;
+  const height = 1200 / gridColumns;
+
+  return (
+    <Images>
+      {images.map((el, index) => (
+        <Item
+          width={width}
+          padding={`${gutter}% ${
+            (index + 1) % gridColumns === 0 ? '0' : gutter
+          }% ${gutter}% ${index % gridColumns === 0 ? '0' : gutter}%`}
+          height={height}
+        >
+          <div>
+            <img
+              key={`https://source.unsplash.com/800x${800 + index}`}
+              src={`https://source.unsplash.com/800x${800 + index}`}
+              title="Image title"
+              alt={'image alt'}
+            />
+          </div>
+        </Item>
+      ))}
+    </Images>
+  );
+};
--- src/MosaicExample.tsx
+++ src/MosaicExample.tsx
@@ -8,7 +8,7 @@ import {Mosaic} from './Mosaic';
 
 const images = Array.from('x'.repeat(40));
 export const MosaicExample: React.FunctionComponent<Props> = ({margin}) => (
-  <div style={{maxWidth: '400px', margin}}>
-    <Mosaic images={images} />
+  <div style={{maxWidth: '1200px', margin}}>
+    <Mosaic gridColumns={5} gridGutter={1} images={images} />
   </div>
 );
